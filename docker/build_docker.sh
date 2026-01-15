86d8ce717fe7a0683024879c3ce5640873833d47
Dongmyeong Lee <dom9316@gmail.com>
2026-01-15 15:35:56 -0600

    fix compose

--- docker/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# usage: ./docker/build_docker.sh [ROS_DISTRO]
-set -euo pipefail
-
-ROS_DISTRO="${1:-humble}"
-
-docker build -f docker/Dockerfile --build-arg ROS_DISTRO="${ROS_DISTRO}" -t "fastlio-ros2:${ROS_DISTRO}" .
